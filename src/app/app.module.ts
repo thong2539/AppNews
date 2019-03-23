8e94166c14ace45e53c2449554c85425185e8da3
thong2539 <phathong5562@gmail.com>
2019-03-23 11:13:59 +0700

    Up popular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { PopularPage } from '../pages/popular/popular';
+import { SciencePage } from '../pages/science/science';
+import { MoviePage } from '../pages/movie/movie';
+import { DetailPage } from '../pages/detail/detail';
+
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    PopularPage,
+    SciencePage,
+    MoviePage,
+    DetailPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule 
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    PopularPage,
+    SciencePage,
+    MoviePage,
+    DetailPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/popular/popular.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+/**
+ * Generated class for the PopularPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-popular',
+  templateUrl: 'popular.html',
+})
+export class PopularPage {
+  popnews :any = 0;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http) {
+    this.http.get('https://www.reddit.com/r/popular/new.json?limit=20')
+    .map(res => res.json()).subscribe(data =>{
+      this.popnews = data.data.children;
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PopularPage');
+  }
+
+}
